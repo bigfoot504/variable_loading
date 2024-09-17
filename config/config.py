733806211861cea4235e23ccdfcc8bad16f47ae0
Robert Locke <robert.m.locke8.civ@us.navy.mil>
2024-09-16 22:27:00 -1000

    Started refactoring to clean up code bloat.

--- config/config.py
+++ config/config.py
@@ -10,9 +10,11 @@ def config_reader_yaml():
     with open(pathYaml, 'r') as file:
         config_dict = yaml.safe_load(file)
 
+    # Convert block volume distribution across weeks into a probability distribution
     for time_tier, vol_distr in config_dict['Volume_Distributions'].items():
         config_dict['Volume_Distributions'][time_tier] = np.array(vol_distr) / sum(vol_distr)
 
+    # Convert weekly volume distribution across days into a probability distribution
     for lift, lift_dict in config_dict['Lifts'].items():
         weekly_distribution = config_dict['Lifts'][lift]['Weekly Distribution']
         config_dict['Lifts'][lift]['Weekly Distribution'] = np.array(weekly_distribution) / sum(weekly_distribution)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from config.config import config_reader_yaml
 from pprint import pprint
 
-rng = np.random.default_rng(123)
+rng = np.random.default_rng()
 
 
 def round_retain_sum(x):
@@ -23,8 +23,38 @@ def round_retain_sum(x):
 
 
 def gen_load(wt_rd_factor, size=None, one_rep_max=None):
+    '''Generate load(s).
 
+    Uses beta distribution so that random load(s) are between 50% and 100%.
+    Beta parameters have been tuned so that 25% of the loads are between
+    50-70%, 50% of the loads are between 70-85%, and 5% of the loads are
+    greater than 85%.
+
+    Parameters:
+    -----------
+    wt_rd_factor : int or float
+        E.g., if 5, will round randomly generated load to nearest 5lbs.
+    size : None, int, or iterable
+        Dimensions of random loads to generate.
+        If None, load is generate as a float.
+        If int, load is generated as a radnom numpy array of that length.
+        If iterable, load is generated as a random numpy array of that shape.
+    one_rep_max : None, int, or float
+        If None, load remains as a percent.
+        If int or float, load is generated as a percentage of 1 rep max.
+
+    Returns:
+    --------
+
+    '''
+    assert type(wt_rd_factor) in [int, float] and (0<wt_rd_factor and wt_rd_factor<=10)
+    assert size is None or type(size) in [int, tuple]
+    assert one_rep_max is None or type(one_rep_max) in [int, float]
+
+    # Generate load as percent
     load = 0.5 + 0.5 * rng.beta(a=7.09, b=7.39, size=size)
+    
+    # Case to change load from percent to weight
     if one_rep_max:
         load *= one_rep_max
 
@@ -37,7 +67,9 @@ def gen_load(wt_rd_factor, size=None, one_rep_max=None):
 
 
 def get_config_data(is_print=False):
-
+    """
+    Get configuration data from the yaml.
+    """
     config_dict = config_reader_yaml()
 
     if is_print:
@@ -47,62 +79,109 @@ def get_config_data(is_print=False):
     return config_dict
 
 
+def make_blocks_data(lifts_dict, vol_distr_dict):
+    # Populate block-deep cycle data
+    for lift, lift_dict in lifts_dict.items():
+        # Array containing volume for each block of the cycle for the lift
+        lift_cycle_blocks_vol = round_retain_sum(
+            vol_distr_dict['Blocks']*lift_dict['Blocks']['Volume']
+        )
+        
+        lifts_dict[lift]['Blocks'].update(
+            {
+                block_num+1: {
+                    'Volume': vol,
+                    'Percent': vol / lift_cycle_blocks_vol.sum()
+                } for block_num, vol in enumerate(lift_cycle_blocks_vol)
+            }
+        )
+    pprint(lifts_dict); input()
+
+
+def make_weeks_data(lifts_dict, vol_distr_dict):
+    # Populate week-deep block data for each lift for each block
+
+    cycle_len = len(vol_distr_dict['Blocks'])
+
+    for block_num in range(1, cycle_len+1):
+        block_distr = rng.permuted(vol_distr_dict['Weeks'])
+        # All lifts follow same volume distribution in splitting block volume over the weeks
+        for lift, lift_dict in lifts_dict.items():
+            # Array containing volume for each week of the block for the lift
+            lift_block_weeks_vol = round_retain_sum(
+                block_distr * lift_dict['Blocks'][block_num]['Volume']
+            )
+            lift_dict['Blocks'][block_num]["Weeks"] = {
+                week_num + 1: {
+                    "Volume": vol,
+                    "Percent": vol / lift_block_weeks_vol.sum(),
+                } for week_num, vol in enumerate(lift_block_weeks_vol)
+            }
+    pprint(lifts_dict); input()
+
+
 def main():  
 
     config_dict = get_config_data()
 
     vol_distr_dict = config_dict['Volume_Distributions']
     lifts_dict     = config_dict['Lifts']
-    cycle_len = len(vol_distr_dict['Cycle'])
 
-    training_days_per_week = max([len(lift_dict['Weekly Distribution']) for lift_dict in lifts_dict.values()])
+    training_days_per_week = max(
+        [
+            len(lift_dict['Weekly Distribution']) for lift_dict in lifts_dict.values()
+        ]
+    )
 
-    schedule_df = pd.DataFrame(columns=['Block', 'Week', 'Day', 'Lift', 'Weight', 'Volume'])
+    schedule_df = pd.DataFrame(
+        columns = ['Block', 'Week', 'Day', 'Lift', 'Weight', 'Volume'],
+    )
 
     # Populate block-deep cycle data
-    for lift, lift_dict in lifts_dict.items():
-        # Array containing volume for each block of the cycle for the lift
-        lift_cycle_blocks_vol = round_retain_sum(vol_distr_dict['Cycle']*lift_dict['Cycle']['Volume'])
-        
-        lifts_dict[lift]['Cycle'].update({'Block '+str(block_num+1):{'Volume':vol,
-                                                                     'Percent':vol/lift_cycle_blocks_vol.sum()} for block_num,vol in enumerate(lift_cycle_blocks_vol)})
-    # pprint(lifts_dict); input()
+    make_blocks_data(lifts_dict, vol_distr_dict)
 
     # Populate week-deep block data for each lift for each block
-    for block_num in range(1, cycle_len+1):
-        block_distr = rng.permuted(vol_distr_dict['Block'])
-        # All lifts follow same volume distribution in splitting block volume over the weeks
-        for lift, lift_dict in lifts_dict.items():
-            # Array containing volume for each week of the block for the lift
-            lift_block_weeks_vol = round_retain_sum(block_distr*lift_dict['Cycle']['Block '+str(block_num)]['Volume'])
-            lifts_dict[lift]['Cycle']['Block '+str(block_num)].update({'Week '+str(week_num+1):{'Volume':vol,
-                                                                                                'Percent':vol/lift_block_weeks_vol.sum()} for week_num,vol in enumerate(lift_block_weeks_vol)})
-    # pprint(lifts_dict); input()
+    make_weeks_data(lifts_dict, vol_distr_dict)
 
     # Populate day-deep week data for each week for each block for each lift
     for lift, lift_dict in lifts_dict.items():
-        for block_name, block_dict in lift_dict['Cycle'].items():
-            if not block_name.startswith('Block'): continue
-            for week_name, week_dict in block_dict.items():
-                if not week_name.startswith('Week'): continue
+        for block_num, block_dict in lift_dict['Blocks'].items():
+            for week_num, week_dict in block_dict["Weeks"].items():
                 week_distr = rng.permuted(lift_dict['Weekly Distribution'])
-                lift_week_days_vol = round_retain_sum(week_distr*week_dict['Volume'])
+                lift_week_days_vol = round_retain_sum(week_distr * week_dict['Volume'])
                 # In case the lift is not trained on every training day
-                day_nums = rng.choice(training_days_per_week, len(lift_dict['Weekly Distribution']), replace=False)+1
-                lifts_dict[lift]['Cycle'][block_name][week_name].update({'Day '+str(day_num):{'Volume':vol,
-                                                                                                'Percent':vol/lift_week_days_vol.sum()} for day_num,vol in zip(day_nums, lift_week_days_vol)})
-                for day_name, day_dict in lifts_dict[lift]['Cycle'][block_name][week_name].items():
-                    if not day_name.startswith('Day'): continue
-                    schedule_df = pd.concat((schedule_df,
-                                             pd.DataFrame({'Block': int(block_name.split()[1]),
-                                                           'Week': int(week_name.split()[1]),
-                                                           'Day': int(day_name.split()[1]),
-                                                           'Lift': lift,
-                                                           'Weight': gen_load(wt_rd_factor=config_dict['Weight_rounding_factor'],
-                                                                              one_rep_max=lift_dict['Max']),
-                                                           'Volume': day_dict['Volume'],
-                                                           }, index=[0])))
-    schedule_df = schedule_df.sort_values(['Block', 'Week', 'Day', 'Lift']).reset_index(drop=True)
+                day_nums = rng.choice(
+                    training_days_per_week,
+                    len(lift_dict['Weekly Distribution']),
+                    replace = False,
+                ) + 1
+                week_dict["Days"] = {
+                    'Day ' + str(day_num): {
+                        'Volume': vol,
+                        'Percent': vol / lift_week_days_vol.sum(),
+                    } for day_num,vol in zip(day_nums, lift_week_days_vol)
+                }
+                for day_num, day_dict in week_dict.items():
+                    schedule_df = pd.concat((
+                        schedule_df,
+                        pd.DataFrame(
+                            {
+                                'Block': block_num,
+                                'Week': week_num,
+                                'Day': day_num,
+                                'Lift': lift,
+                                'Weight': gen_load(
+                                    wt_rd_factor = config_dict['Weight_rounding_factor'],
+                                    one_rep_max = lift_dict['Max']
+                                ),
+                                'Volume': day_dict['Volume'],
+                            },
+                            index = [0],
+                        )
+                    ))
+    schedule_df.sort_values(
+        ['Block', 'Week', 'Day', 'Lift'],
+    ).reset_index(drop=True, inplace=True)
 
     for block_num in schedule_df['Block'].unique():
         for lift in schedule_df['Lift'].unique():
